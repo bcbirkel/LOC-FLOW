7654a68f771f5312ad4971b02cbeba723511ca19
Brianna Birkel <birkel@usc.edu>
2025-12-01 18:07:12 -0800

    feat: Use station elevation and exclude S-phase from single-event location

--- location/VELEST/convertformat_updated.pl
+++ location/VELEST/convertformat_updated.pl
@@ -31,7 +31,7 @@ if (-e "final.CNV"){`rm velest.* initial.cat final.CNV`;}
 $reflon = $reflon*-1; # NOTE: western lon is positive in velest!!! e.g., 117.5W => 117.5
 $iusestacorr = 1; # station correction used or not
 $zmin = -5.0; # was -0.2 # the smallest depth allowed (e.g., -0.2 -> above the sea level)
-$iuseelev = 0; # station elevation used or not (recommend: 0)
+$iuseelev = 1; # station elevation used or not (recommend: 0)
 $lowvelocity = 0; # any low velocity layer in the region (recommend: 0)
 $vthet = 10; # damping of velocity
 $stathet = 1; # damping of station correction
@@ -240,7 +240,7 @@ foreach $file(@par){
         } elsif ($phase eq 'S') {
             # In single-event mode, VELEST sets w=0.0 for ipwt > 4,
             # effectively excluding them from location.
-            $iwt = 1;
+            $iwt = 5;
         }
         
         $sta_short = sprintf("%-4s", $sta_short); # Pad to 4 chars
